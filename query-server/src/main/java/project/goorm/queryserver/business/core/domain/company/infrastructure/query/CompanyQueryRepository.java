cc81a7259d2f68977236e60dadf7a2d74efcbe7b
kogo28 <kogo28@naver.com>
2023-03-24 01:53:52 +0900

    build, feature: CompanyQueryRepository 추가, CompanyTypeException 예외 추가  - CompanyQueryRepository   *companyId로 Company를 찾는 쿼리 추가   *companyName으로 Company를 찾는 쿼리 추가   *Company Table에 등록된 모든 Company를 반환하는 쿼리 추가  - CompanyTypeException   *companyId 또는 companyName으로 Company를 찾을 때 등록되지 않은 값을   입력했을 때 발생하는 예외 타입 추가   *Company Table이 비었을 때 Company 요청에 발생하는 예외 타입 추가

---
+++ query-server/src/main/java/project/goorm/queryserver/business/core/domain/company/infrastructure/query/CompanyQueryRepository.java
@@ -0,0 +1,35 @@
+package project.goorm.queryserver.business.core.domain.company.infrastructure.query;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+import project.goorm.queryserver.business.core.domain.company.entity.Company;
+
+import java.util.List;
+import java.util.Optional;
+
+import static project.goorm.queryserver.business.core.domain.company.entity.QCompany.company;
+
+@Repository
+public class CompanyQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public CompanyQueryRepository(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    public Optional<Company> findCompanyById(Long companyId) {
+        return Optional.ofNullable(queryFactory.selectFrom(company)
+                .where(company.companyId.eq(companyId)).fetchOne());
+    }
+
+    public Optional<Company> findCompanyByName(String companyName) {
+        return Optional.ofNullable(queryFactory.selectFrom(company)
+                .where(company.companyName.eq(companyName)).fetchOne());
+    }
+
+    public Optional<List<Company>> findCompanyAll() {
+        return Optional.ofNullable(queryFactory.selectFrom(company)
+                .fetch());
+    }
+}
