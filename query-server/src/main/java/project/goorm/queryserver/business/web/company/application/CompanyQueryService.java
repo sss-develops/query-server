662b6b1910761c71986ae8e20b2a9cb34e8a8eeb
kogo28 <kogo28@naver.com>
2023-03-24 02:02:27 +0900

    build, refactor, feature: Company Service와 API, ResponseDTO를 추가  - CompanyResponse   *취약점이 될 수 있는 companyId를 제외한 Company 엔티티를 반환할 DTO 추가  - CompanyQuerySerivce   *기존에 정의한 서비스에 Company를 찾는 비즈니스 로직 추가   *회사 ID로 등록된 회사를 찾는 findCompanyById 추가   *회사 이름으로 등록된 회사를 찾는 findCompanyByName 추가   *Company Tabled에 등록된 모든 회사를 찾는 findCompanyAll 추가   *Entity를 ResponseDTO(CompanyResponse)로 변환하여 반환  - API   *companyId로 회사 정보를 반환해주는 CompanyQueryByIdAPI 추가   *companyName으로 회사 정보를 반환해주는 CompanyQueryByNameAPI 추가   *Company Table에 등록된 모든 회사를 리스트로 반환해주는 CompanyQueryAll 추가   *Get Type Method로 정의된 API들은 Test Code 작성 및 Test 이후 Post Type Method로 리팩토링

--- query-server/src/main/java/project/goorm/queryserver/business/web/company/application/CompanyQueryService.java
+++ query-server/src/main/java/project/goorm/queryserver/business/web/company/application/CompanyQueryService.java
@@ -1,7 +1,17 @@
 package project.goorm.queryserver.business.web.company.application;
 
 import org.springframework.stereotype.Service;
-import project.goorm.queryserver.business.core.company.infrastructure.query.CompanyQueryRepository;
+import org.springframework.transaction.annotation.Transactional;
+import project.goorm.queryserver.business.core.domain.company.entity.Company;
+import project.goorm.queryserver.business.core.domain.company.infrastructure.query.CompanyQueryRepository;
+import project.goorm.queryserver.business.web.company.presentation.response.CompanyResponse;
+import project.goorm.queryserver.common.exception.common.SSSTeamException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static project.goorm.queryserver.business.core.domain.company.CompanyTypeException.COMPANY_IS_EMPTY_EXCEPTION;
+import static project.goorm.queryserver.business.core.domain.company.CompanyTypeException.COMPANY_NOT_FOUND_EXCEPTION;
 
 @Service
 public class CompanyQueryService {
@@ -10,5 +20,26 @@ public class CompanyQueryService {
 
     public CompanyQueryService(CompanyQueryRepository companyQueryRepository) {
         this.companyQueryRepository = companyQueryRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public CompanyResponse findCompanyById(Long companyId) {
+        return new CompanyResponse(companyQueryRepository.findCompanyById(companyId)
+                .orElseThrow(() -> SSSTeamException.of(COMPANY_NOT_FOUND_EXCEPTION)));
+    }
+
+    @Transactional(readOnly = true)
+    public CompanyResponse findCompanyByName(String companyName) {
+        return new CompanyResponse(companyQueryRepository.findCompanyByName(companyName)
+                .orElseThrow(() -> SSSTeamException.of(COMPANY_NOT_FOUND_EXCEPTION)));
+    }
+
+    @Transactional(readOnly = true)
+    public List<CompanyResponse> findCompanyAll() {
+        List<Company> findCompany = companyQueryRepository.findCompanyAll()
+                .orElseThrow(() -> SSSTeamException.of(COMPANY_IS_EMPTY_EXCEPTION));
+        List<CompanyResponse> companyResponsesList = findCompany.stream()
+                .map(c -> new CompanyResponse(c)).collect(Collectors.toList());
+        return companyResponsesList;
     }
 }
