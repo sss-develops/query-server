ef4af96387f68c1421134b3872acffdcc4c6e27d
kogo28 <kogo28@naver.com>
2023-03-30 19:05:51 +0900

    build, refactor: 회사 테스트에 사용할 파일 추가 및 수정  - 파일 추가   *회사 테스트 픽스쳐 추가   *데이터 저장에 사용될 회사 JpaRepository 추가  - 파일 리팩토링   *PersistenceHelper에 회사 관련 내용 추가   *픽스쳐에서 생성할 수 있도록 회사 엔티티에 생성자 추가

--- query-server/src/test/java/project/goorm/queryserver/test/helper/helper/PersistenceHelper.java
+++ query-server/src/test/java/project/goorm/queryserver/test/helper/helper/PersistenceHelper.java
@@ -2,6 +2,8 @@ package project.goorm.queryserver.test.helper.helper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import project.goorm.queryserver.business.core.domain.company.entity.Company;
+import project.goorm.queryserver.business.core.domain.company.infrastructure.command.CompanyJpaRepository;
 import project.goorm.queryserver.business.core.domain.news.entity.News;
 import project.goorm.queryserver.business.core.domain.news.infrastructure.command.NewsJpaRepository;
 
@@ -11,8 +13,15 @@ public class PersistenceHelper {
     @Autowired
     private NewsJpaRepository newsJpaRepository;
 
+    @Autowired
+    private CompanyJpaRepository companyJpaRepository;
+
     public News persist(News news) {
         return newsJpaRepository.save(news);
+    }
+
+    public Company persist(Company company) {
+        return companyJpaRepository.save(company);
     }
 
     public News findNewsById(Long newsId) {
