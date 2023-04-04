24b5673d34d95842a1bdfd3b19d1bd088d501d1e
Bangseungjae <tmdwo5825@naver.com>
2023-04-04 19:21:47 +0900

    refactor: 필요없는 import제거한다.

--- query-server/src/test/java/project/goorm/queryserver/common/configuration/rdb/AbstractContainerTestBase.java
+++ query-server/src/test/java/project/goorm/queryserver/common/configuration/rdb/AbstractContainerTestBase.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.MySQLContainer;
-import project.goorm.queryserver.common.configuration.redis.RedisInitialization;
 
 @SpringBootTest
 @ActiveProfiles("test")
