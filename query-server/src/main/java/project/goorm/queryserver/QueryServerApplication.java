dd4f652754f51fd664d3b9f81125e8e956681414
Bangseungjae <tmdwo5825@naver.com>
2023-04-08 00:51:06 +0900

    build: Config-server와 연동하며 RefreshScope을 추가한다.

--- query-server/src/main/java/project/goorm/queryserver/QueryServerApplication.java
+++ query-server/src/main/java/project/goorm/queryserver/QueryServerApplication.java
@@ -3,7 +3,9 @@ package project.goorm.queryserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class QueryServerApplication {
 
